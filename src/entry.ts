805599f2004d8657f429559a7b03259bfb223a7d
weibin <372618291@qq.com>
2023-12-26 17:49:57 +0800

    feat: 添加样式系统UnoCss

--- src/entry.ts
+++ src/entry.ts
@@ -6,7 +6,6 @@ export { RzButton }
 
 // 编写一个插件实现install方法
 export default {
-
   install(app: App) {
     // 注册组件
     app.component(RzButton.name, RzButton)
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,30 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-const rollupOptions = {
-  external: ["vue"],
-  output: {
-    globals: {
-      vue: "Vue",
-    },
-  },
-};
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import Unocss from "./config/unocss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    rollupOptions,
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
     minify: false,
     lib: {
       entry: "./src/entry.ts",
       name: "RzUI",
       fileName: "rz-ui",
       // 导出模块格式
-      formats: ["es", "umd","iife"],
+      formats: ["es", "umd", "iife"],
     },
   },
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vueJsx(),
+    // 添加UnoCSS插件
+    Unocss(),
+  ],
 });
