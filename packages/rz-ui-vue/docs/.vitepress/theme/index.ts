38dde93f3632f71fb11d9e1ef7e7560028b644e2
weibin <372618291@qq.com>
2024-01-23 15:51:27 +0800

    feat: 将docs放在rz-ui-vue内

---
+++ packages/rz-ui-vue/docs/.vitepress/theme/index.ts
@@ -0,0 +1,17 @@
+import DefaultTheme from 'vitepress/theme'
+import Demo from 'vitepress-theme-demoblock/dist/client/components/Demo.vue'
+import DemoBlock from 'vitepress-theme-demoblock/dist/client/components/DemoBlock.vue'
+import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
+// import RzUI from "rz-ui-vue";
+// import "uno.css";
+// import "rz-ui-vue/dist/entry.css";
+import RzUI from '../../../src/entry'
+
+export default {
+  ...DefaultTheme,
+  enhanceApp(ctx) {
+    ctx.app.component('Demo', Demo)
+    ctx.app.component('DemoBlock', DemoBlock)
+    ctx.app.use(RzUI)
+  }
+}
