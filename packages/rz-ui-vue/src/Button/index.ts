58287a11625f101acdac377f4ab23382a841e8e5
weibin <372618291@qq.com>
2024-01-23 10:41:09 +0800

    feat: 实现组件库按需引入

--- packages/rz-ui-vue/src/Button/index.ts
+++ packages/rz-ui-vue/src/Button/index.ts
@@ -1,14 +1,13 @@
+import Button from './Button'
 import { App } from 'vue'
-import { Button } from './Button'
 import 'uno.css'
 
-// 单独导出组件
+// 导出Button组件
 export { Button }
 
-// 编写一个插件实现install方法
+// 导出Vue插件
 export default {
   install(app: App) {
-    // 注册组件
     app.component(Button.name, Button)
   }
 }
